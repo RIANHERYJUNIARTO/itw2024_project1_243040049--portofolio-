3260219c4637904ddbaff03f0ae40ac0a746017a
RIANHERYJUNIARTO <rianherijr@gmail.com>
2025-01-06 23:49:48 +0700

    revisi

--- js/script.js
+++ js/script.js
@@ -7,22 +7,22 @@ menuicon.addEventListener("click", () => {
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header apaweh a");
 
-window.onscroll = () => {
-  let scrollPosition = window.scrollY;
+// window.onscroll = () => {
+//   let scrollPosition = window.scrollY;
 
-  sections.forEach((sec) => {
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
+//   sections.forEach((sec) => {
+//     let offset = sec.offsetTop;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
 
-    if (scrollPosition >= offset && scrollPosition < offset + height) {
-      navLinks.forEach((link) => {
-        link.classList.remove("active");
-      });
+//     if (scrollPosition >= offset && scrollPosition < offset + height) {
+//       navLinks.forEach((link) => {
+//         link.classList.remove("active");
+//       });
 
-      document
-        .querySelector(`header apaweh a[href="#${id}"]`)
-        .classList.add("active");
-    }
-  });
-};
+//       document
+//         .querySelector(`header apaweh a[href="#${id}"]`)
+//         .classList.add("active");
+//     }
+//   });
+// };
